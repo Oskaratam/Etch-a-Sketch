b8846f038565fb21774ea4876983cdb14fd619a2
Oskaratam <oskaratamanenko2@gmail.com>
2023-07-31 13:43:02 +0300

    added dynamic sizing with a range slider

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
 /* 
 1.Create a layout +
 2.Make a grid paintArea +
-3.Add ability to paint
+3.Add ability to paint +
 4.Add slider to change size of the paintArea
 5.Add "clear" button 
 6.Add eraser 
 7.Add button to switch colors
 */
 
-const paintArea = document.getElementById('paintArea');
+const body = document.querySelector('body'),
+      paintArea = document.getElementById('paintArea'),
+      sliderSize = document.getElementById('sliderSize'),
+      currentSize = document.getElementById('currentSize');
 
-let = columnsCount = 20,
-      rowsCount = 20;
-      paintColor = 'red';
+let = columnsCount = 16,
+      rowsCount = 16;
+      paintColor = 'yellow';
 
 const createGrid = () => {
     for(i = 1; i <= columnsCount; i++){
@@ -23,21 +26,33 @@ const createGrid = () => {
         for (j = 1; j <= rowsCount; j++){
             let newBox = document.createElement('div');
             newBox.classList.add(`box`)
-            newBox.setAttribute('style', `border: 1px solid black; width:50px; height:${700 / columnsCount}px`);
+            newBox.setAttribute('style', `border: 1px solid black; width:100px; height:${700 / columnsCount}px`);
             newColumn.appendChild(newBox);     
         } 
     }
-
+    let MouseDown;
+    body.addEventListener('mousedown', () => {MouseDown = 1});
+    body.addEventListener('mouseup', () => {MouseDown = 0});
+    document.querySelectorAll('.box').forEach(box => {
+        box.addEventListener('mouseover', () => {if (MouseDown === 1) {
+            box.style.backgroundColor = paintColor}
+        })
+        box.addEventListener('click', () => {box.style.backgroundColor = paintColor})
+    }
+    )
 }
 createGrid()
 
+//Sizing with slider
+sliderSize.addEventListener('input', () => {
+    columnsCount = sliderSize.value;
+    rowsCount = sliderSize.value;
+    paintArea.innerHTML = null; 
+    createGrid();
+    currentSize.innerHTML = `${rowsCount}x${columnsCount}`;
+})
 
-let MouseDown;
-document.querySelectorAll('.box').forEach(box => {
-    paintArea.addEventListener('mousedown', () => {MouseDown = 1});
-    paintArea.addEventListener('mouseup', () => {MouseDown = 0});
-    box.addEventListener('mouseover', () => {if (MouseDown === 1) {
-        box.style.backgroundColor = paintColor}
-    })
-}
-)
+
+
+
+
